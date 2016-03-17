52a8feedd7003a0949a4ca0fa356125ae7347ddf
Nata1ika <gulyaeva>
2016-03-17 06:16:49 +0300

    скроллу добавлена инерция, добавление элементов покадрово. заметна задержка если добавить 1000 элементов сразу

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -11,8 +11,10 @@ public class Controller : MonoBehaviour
 	
 	public void OnClickCreateButton()
 	{
-		_scroll.AddRandomElements(100);
-		_scroll.GoToElement(98);
+		//_scroll.AddRandomElements(3, false);
+		//_scroll.AddRandomElements(20, true);
+		//_scroll.GoToElement(30);
+		StartCoroutine(_scroll.AddRandomElements(1000, 99, 50)); //добавить 1000 элементов, 99 из них будут выше выбранного, по 50 элеметнов за фрейм
 	}
 
 	public void OnClickClearButton()
--- Assets/Scripts/ScrollController.cs
+++ Assets/Scripts/ScrollController.cs
@@ -17,42 +17,72 @@ public class ScrollController : MonoBehaviour
 		}
 	}
 
-	public void AddElement(Grid grid)
+	public void AddElement(Grid grid, bool top) //grid - данные для строки, top - место добавления строки
 	{
 		if (_scroll == null)
 		{
 			_scroll = new List<Element>();
 		}
 
-		GameObject obj =  GameObject.Instantiate(_prefabElement);
-		if (obj != null)
+		GameObject obj = CreateElement(grid);
+		if (top)
 		{
-			obj.transform.SetParent(_content);
-			obj.GetComponent<ScrollElement>().Set(grid);
+			_scroll.Insert(0, new Element(obj, grid));
+			obj.transform.SetAsFirstSibling(); //двигаем элемент
+			GoToElement(1, true); //двигаем скролл на 1 элемент вверх
 		}
-
-		_scroll.Add(new Element(obj, grid));
+		else
+		{
+			_scroll.Add(new Element(obj, grid));
+		}
 	}
 
-	public void AddRandomElement()
-	{
-		Grid grid = new Grid();
-		AddElement(grid);
-	}
-
-	public void AddRandomElements(int count)
+	public void AddRandomElements(int count, bool top) //добавить count случайных элементов вверху (top == true) или внизу (false) скролла
 	{
 		if (count > 0)
 		{
 			for (int i=0; i<count; i++)
 			{
-				AddRandomElement();
+				Grid grid = new Grid();
+				AddElement(grid, top);
 			}
 		}
 	}
 
+	public IEnumerator AddRandomElements(int count, int index, int elementForFrame) //общее количество элементов, элемент на котором остановится скоролл, количество элементов за кадр
+	{
+		_coroutineProcess = true;
+		//добавляем по порядку элементы, исключая первые
+		for (int i = (count-index) / elementForFrame + ((count-index) % elementForFrame > 0 ? 1 : 0) - 1; i >= 0; i--)
+		{
+			if (_coroutineProcess)
+			{
+				AddRandomElements((i == 0 && (count-index) % elementForFrame > 0) ? ((count-index) % elementForFrame) : elementForFrame, false);
+			}
+			yield return null;
+		}
+		//добавляем элементы вверх
+		if (index > 0)
+		{
+			for (int i = index / elementForFrame + (index % elementForFrame > 0 ? 1 : 0) - 1; i >= 0; i--)
+			{
+				if (_coroutineProcess)
+				{
+					AddRandomElements((i == 0 && index % elementForFrame > 0) ? (index % elementForFrame) : elementForFrame, true);
+				}
+				yield return null;
+			}
+		}
+		_coroutineProcess = false;
+	}
+
 	public void Clear()
 	{
+		if (_coroutineProcess)
+		{
+			_coroutineProcess = false;
+		}
+
 		if (_scroll != null && _scroll.Count > 0)
 		{
 			for (int i=0; i<_scroll.Count; i++)
@@ -63,37 +93,87 @@ public class ScrollController : MonoBehaviour
 		}
 	}
 
-	public void GoToElement(int index)
+	public void GoToElement(int index) //движение к заданному индкексу
 	{
 		if (_scroll != null && index < _scroll.Count)
 		{
 			RectTransform rect = _content.gameObject.GetComponent<RectTransform>();
 			if (rect != null)
 			{
-				//рассчитаем максимальную высоту, на которую можно поднять скролл. последний элемент скролла не может быть выше окончания скролла
-				RectTransform parentRect = rect.parent.gameObject.GetComponent<RectTransform>();
-				VerticalLayoutGroup layoutGroup = _content.gameObject.GetComponent<VerticalLayoutGroup>();
-				LayoutElement layout = _prefabElement.GetComponent<LayoutElement>();
-
-				if (parentRect != null && layoutGroup != null && layout != null)
+				float height = Mathf.Min(GetMaxHeight(), index * _elementSizeWithSpace);
+				if (height < 0f)
 				{
-					float elementSize = layout.minHeight;
-					float maxHeight = layoutGroup.padding.top + layoutGroup.padding.bottom + layoutGroup.spacing * (_scroll.Count - 1) + elementSize * _scroll.Count - parentRect.rect.height;
-
-					float height = Mathf.Min(index * (elementSize + layoutGroup.spacing), maxHeight);//верхний элемент не может опуститься ниже
-					if (height < 0f)
-					{
-						height = 0f;
-					}
-
-					rect.anchoredPosition = new Vector2(rect.anchoredPosition.x, height);
-				}
+					height = 0f;
+				}				
+				rect.anchoredPosition = new Vector2(rect.anchoredPosition.x, height);
 			}		
 		}
+	}
+
+	public void GoToElement(int index, bool top) //движение вверх/вниз на количество элементов
+	{
+		if (_scroll != null && index < _scroll.Count)
+		{
+			RectTransform rect = _content.gameObject.GetComponent<RectTransform>();
+			if (rect != null)
+			{
+				float height = GetMaxHeight();
+
+				float delta = (top ? 1f : (-1f)) * index * _elementSizeWithSpace;
+				height = Mathf.Min(height, rect.anchoredPosition.y + delta);
+				if (height < 0f)
+				{
+					height = 0f;
+				}				
+				rect.anchoredPosition = new Vector2(rect.anchoredPosition.x, height);
+			}
+		}
+	}
+
+	GameObject CreateElement(Grid grid)
+	{
+		GameObject obj =  GameObject.Instantiate(_prefabElement);
+		if (obj != null)
+		{
+			obj.transform.SetParent(_content);
+			obj.GetComponent<ScrollElement>().Set(grid);
+		}
+		return obj;
+	}
+
+	float GetMaxHeight() //максимальная высота на которую можно поднять скролл. последний элемент может находиться не выше нижней границы (при уловии достаточного количества элементов))
+	{
+		if (_scroll == null || _scroll.Count == 0)
+		{
+			return 0;
+		}
+
+		if (_constParamForMaxHeight < 0 || _elementSizeWithSpace < 0)//не посчитан постоянный параметр
+		{
+			RectTransform rect = _content.gameObject.GetComponent<RectTransform>();
+			RectTransform parentRect = rect.parent.gameObject.GetComponent<RectTransform>();
+			VerticalLayoutGroup layoutGroup = _content.gameObject.GetComponent<VerticalLayoutGroup>();
+			LayoutElement layout = _prefabElement.GetComponent<LayoutElement>();
+
+			if (rect == null || parentRect == null || layoutGroup == null || layout == null)
+			{
+				return 0f;
+			}
+
+			_elementSizeWithSpace = layout.minHeight + layoutGroup.spacing;
+			_constParamForMaxHeight = layoutGroup.padding.top + layoutGroup.padding.bottom - layoutGroup.spacing - parentRect.rect.height;
+		}
+
+		return _elementSizeWithSpace * _scroll.Count + _constParamForMaxHeight;
 	}
 
 	[SerializeField] Transform	_content;
 	[SerializeField] GameObject _prefabElement;
 
 	List<Element>				_scroll;
+
+	float						_constParamForMaxHeight = -1f; //постоянные параменты для подсчета maxHeight
+	float						_elementSizeWithSpace = -1f; //высота елемента с промежутком между элементами
+	bool						_coroutineProcess = false;
+
 }
